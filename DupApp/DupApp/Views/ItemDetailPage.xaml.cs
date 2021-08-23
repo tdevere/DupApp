b8f9ff18d19af8353830cf4cf1ab17606da4b951
tdevere <tdevere@microsoft.com>
2021-08-23 13:18:58 -0500

    Add project files.

---
+++ DupApp/DupApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using DupApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace DupApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ DupApp/DupApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using DupApp.Models;
+using DupApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DupApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
